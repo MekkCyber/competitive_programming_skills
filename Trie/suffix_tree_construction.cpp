24e649a5cad7a91fc34f0d1eab7d3be7b424d9b7
MekkCyber <mekk.cyber@gmail.com>
2023-09-30 18:14:36 +0200

    suffix_trie_construction

---
+++ Trie/suffix_tree_construction.cpp
@@ -0,0 +1,53 @@
+#include <unordered_map>
+using namespace std;
+
+class TrieNode {
+ public:
+  unordered_map<char, TrieNode*> children;
+};
+
+class SuffixTrie {
+ public:
+  TrieNode* root;
+  char endSymbol;
+
+  SuffixTrie(string str) {
+    this->root = new TrieNode();
+    this->endSymbol = '*';
+    this->populateSuffixTrieFrom(str);
+  }
+
+  void populateSuffixTrieFrom(string str) {
+    for (int i = 0; i<str.length(); i++){
+      string substring = str.substr(i);
+      insert(substring);
+    }
+  }
+  void insert(string str){
+    TrieNode* currentNode = root;
+    for (int i = 0; i<str.length(); i++){
+      if (currentNode->children.find(str[i])==currentNode->children.end()){
+        currentNode->children[str[i]] = new TrieNode();
+      }
+      currentNode = currentNode->children[str[i]];
+    }
+    if (currentNode->children.find(endSymbol)==currentNode->children.end()){
+        currentNode->children[endSymbol] = nullptr;
+    }
+  }
+  bool contains(string str) {
+    int i = 0;
+    TrieNode* currentNode = root;
+    while (i<str.length()){
+      if (currentNode->children.find(str[i])==currentNode->children.end()){
+        return false;
+      }
+      currentNode = currentNode->children[str[i]];
+      i++;
+    }
+    if (currentNode->children.find(endSymbol)==currentNode->children.end()){
+      return false;
+    }
+    return true;
+  }
+};

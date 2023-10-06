e93749aa273daa98c25b3e22053bacab88bb358e
MekkCyber <mekk.cyber@gmail.com>
2023-10-06 23:15:54 +0200

    bisect

---
+++ Sorts/using_bised_left.py
@@ -0,0 +1,19 @@
+'''
+https://www.hackerrank.com/challenges/fraudulent-activity-notifications/problem?isFullScreen=true
+'''
+from bisect import bisect_left
+
+def activityNotifications(expenditure, d):
+    k = 0
+    sorted_trail = sorted(expenditure[:d])
+    
+    for i in range(len(expenditure) - d):
+        median = sorted_trail[d // 2] if d % 2 == 1 else (sorted_trail[d // 2 - 1] + sorted_trail[d // 2]) / 2
+        if expenditure[i+d] >= 2 * median:
+            k += 1
+        index = bisect_left(sorted_trail, expenditure[i])
+        sorted_trail.pop(index)
+        index = bisect_left(sorted_trail, expenditure[i+d])
+        sorted_trail.insert(index, expenditure[i+d])
+    
+    return k

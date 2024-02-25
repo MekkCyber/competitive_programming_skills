cd0d69f213a24bf614dda0692443480f4b7bd754
Mohamed Mekkouri <93391238+MekkCyber@users.noreply.github.com>
2024-02-25 22:49:54 +0100

    Update longest_subarray_with_sum.cpp

--- Arrays/longest_subarray_with_sum.cpp
+++ Arrays/longest_subarray_with_sum.cpp
@@ -1,5 +1,5 @@
 /*
-    Write a function that takes in a non-empty array of non-negative integers and a non-negative integer
+    Write a function that takes in a non-empty array of non-negative integers and a non-negative integer 
     representing a target sum. The function should find the longest subarray where the values collectively
     sum up to equal the target sum. Return an array containing the starting index and ending index of this subarray,
     both inclusive.

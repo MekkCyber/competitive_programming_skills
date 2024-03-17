b6d5eab5ab32797cfc30a0736a66e858aeb1a8f3
Mohamed Mekkouri <93391238+MekkCyber@users.noreply.github.com>
2024-03-17 23:13:56 +0100

    Update kadanes.cpp

--- Known Algos/kadanes.cpp
+++ Known Algos/kadanes.cpp
@@ -4,7 +4,6 @@
     non-empty subarray of the input array. A subarray must only contain adjacent
     numbers (numbers next to each other in the input array).
 */
-
 /*
     The intuition is that we need to iterate over the array, and keep a running sum, and whenever the sum is <0
     it means we no longer need that part, because it will just make the next part of the array worse
